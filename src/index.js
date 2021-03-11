4b05e29457c542a7b736a6d34f7b534c397ee7f4
Smogo <aurelien.genois@gmail.com>
2021-03-11 12:18:57 +0100

    modules files and test for update tasks priorities/states

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('It\'s init!');
+import {task, taskProto} from './modules/tasks-manager.js'
+
+
+let testTask = task('Test Task', 30, 'fe', 4)
+let task2 = task('task 2', 1)
+
+taskProto.addTaskPriority(4,'VeryLow') // with position and label of new State/priority
+taskProto.addTaskState(1,'Done Well') // function indepent from task factory
+
+
+console.log(testTask, task2)
+console.log(testTask.getState(), task2.getState())
