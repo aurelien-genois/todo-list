11bad190cc7396c7dfcd335883fb39e6c7c6994e
Smogo <aurelien.genois@gmail.com>
2021-03-12 15:46:09 +0100

    create manageProject module

--- src/modules/projects-manager.js
+++ src/modules/projects-manager.js
@@ -1,6 +1,8 @@
 import {task} from './tasks-manager.js'
+import {dom} from './dom-integration.js'
 
-const project = (title, desc) => {
+
+const project = (title, desc = '') => {
     let tasks = [];
     const proto = {
         getTitle: () => title,
@@ -16,16 +18,40 @@ const project = (title, desc) => {
         createTask(title, dueDate, desc, priorityLevel) { 
             // create and keep a task in project tasks[]
             this.tasks.push(task(title, dueDate, desc, priorityLevel));
+            // dom.appendTasks(this.tasks);
         },
-        deleteTask(taskSelected) {
+        deleteTask(taskSelected) { 
+            // can be optimize with unique function for both deleteTask and deleteProject with deleteItem(array, item)
             const taskId = tasks.indexOf(taskSelected);
             tasks.splice(taskId, 1);
+            // dom.appendTasks(this.tasks);
         }
     };
     return Object.assign(Object.create(proto), {tasks});
 }
 // (NTH function setProject for change the project for one task)
 
+// to be moved in right place or condense in one module 'manageProjects'
+const manageProjects = (() => {
+    let _projects = [];
+    const createProject = (title,desc) => {
+        const newProject = project(title, desc);
+        _projects.push(newProject)
+        dom.appendProjectsTabs(manageProjects.getProjects());
+        return newProject;
+    };
+    const deleteProject = (projectSelected) => {
+        const projectId = _projects.indexOf(projectSelected);
+        _projects.splice(projectSelected, 1);
+        dom.appendProjectsTabs(manageProjects.getProjects());
+        return projectSelected;
+    };
+    const getProjects = () => _projects;
+
+    return {createProject, deleteProject, getProjects};
+})();
+
 export {
-    project
+    project,
+    manageProjects
 }
